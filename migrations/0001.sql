cd49106f4a3901901004012b734b8d0c73694175
private-mains <sgt.brittanycruz0@gmail.com>
2026-03-24 00:16:39 +0100

    Update 0001.sql

--- migrations/0001.sql
+++ migrations/0001.sql
@@ -1,13 +1,17 @@
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
   id TEXT PRIMARY KEY,
-  email TEXT UNIQUE,
-  password_hash TEXT,
-  created_at TEXT
+  email TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  created_at TEXT NOT NULL
 );
 
 CREATE TABLE messages (
   id TEXT PRIMARY KEY,
-  user_id TEXT,
-  body TEXT,
-  created_at TEXT
+  user_id TEXT NOT NULL,
+  body TEXT NOT NULL,
+  created_at TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
